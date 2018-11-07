8db506e2c8eb1e74fae3b959f86975e8e2147c71
Deyvid Franklin <deyvid.fk@gmail.com>
2018-11-07 20:29:07 -0200

    correção  - As tarefas terminadas aparecem riscadas

--- src/controllers/TaskListing.js
+++ src/controllers/TaskListing.js
@@ -2,11 +2,15 @@
   var appModule = angular.module("Tarefas");
 
   appModule.controller("TaskListingCtrl", [
-    "$taskDao",
-    function($taskDao) {
+    "$taskDao","$dateUtils",
+    function($taskDao, $dateUtils) {
       var vm = this;
 
       vm.tasks = [];
+
+      vm.dateExpired = function(date){
+        return ($dateUtils.compare(date, new Date()) === -1)
+      }
 
       $taskDao.getTasks().then(
         function(tasks) {
